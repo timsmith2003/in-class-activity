b7fdde0c805ea6d5c2c0ba97f5ab9a70b8e31b23
osromeo <Owen.Romeo@uvm.edu>
2025-04-01 10:32:10 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,21 @@
+#Opens a file
 with open("numbers.html", "w") as f:
+    #write to file
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    #loop
     for i in range(1, 50):
         if i % 2 == 0:
+            #write this if even
             g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         if i % 3 == 0:
+            #write this if multiple of three
             g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+            #write this at end
     f.write("</table>\n</body>\n</html>")
 
+#write to this file
 with open("numbers.html"") as f:
+          #read the file
     print(f.read())
     
