c1cf20c395b409926bcdaef8c2bdd8b375297faf
timsmith2003 <148157082+timsmith2003@users.noreply.github.com>
2025-04-01 10:38:07 -0400

    Update rgb_to_hex.cc
    Broken

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -12,7 +12,7 @@ std::string rgb_to_hex(int r, int g, int b)
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
-    return ss.str();
+    
 }
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
